d83d21e49ae4acb16075974d8720bfe77bfe1ece
abhirammanoh <krishnaavast@gmail.com>
2023-02-26 23:10:16 +0530

    New debug added

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]){
         System.out.println("This is from java");
 		System.out.println("second message");
+		System.out.println("Third Message");
     }
 }
